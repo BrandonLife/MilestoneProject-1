97b315771e9538d24cecc1988761e1ed18ba51d4
Brandon Life <blife1119@gmail.com>
2023-06-28 08:40:51 -0400

    made changes to math.js

--- Math.js
+++ Math.js
@@ -2,11 +2,11 @@ export function math() {
   let operands = ["+", "-", "/", "x"];
   let questionBox = document.createElement("div");
   let answerInput = document.createElement("input");
+  answerInput.setAttribute("id", "userInput");
   let generateProblemBtn = document.createElement("button");
-  let userAnswer, correctAnswer;
-
-  userAnswer = answerInput.value;
-  generateProblemBtn.textContent = "Generate problem";
+  let correctAnswer;
+  let userAnswer = Number(answerInput.value);
+  generateProblemBtn.textContent = "Generate Problem/Submit";
   generateProblemBtn.style.position = "absolute";
   generateProblemBtn.style.top = 40 + "%";
   generateProblemBtn.style.left = 47.5 + "%";
@@ -14,16 +14,10 @@ export function math() {
   document.body.append(generateProblemBtn);
   let result = document.createElement("div");
   let score = 0;
+  console.log(userAnswer);
   generateProblemBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    generateProblemBtn.style.display = "none";
-    let submitButton = document.createElement("button");
-    submitButton.style.position = "absolute";
-    submitButton.style.top = 40 + "%";
-    submitButton.style.left = 47.5 + "%";
-    submitButton.style.zIndex = 2;
-    submitButton.textContent = "Submit Answer";
-    document.body.append(submitButton);
+
     let randomNum1 = Math.floor(Math.random() * 11);
     let randomNum2 = Math.floor(Math.random() * 11);
     let randomOperandGeneration = Math.floor(Math.random() * 4) + 0;
@@ -42,8 +36,6 @@ export function math() {
       questionBox.textContent = `What is ${randomNum1}${operandSymbol}${randomNum2}`;
       correctAnswer = randomNum1 / randomNum2;
     }
-
-    userAnswer = Number(userAnswer);
     console.log(userAnswer, correctAnswer);
 
     questionBox.style.position = "absolute";
@@ -58,6 +50,7 @@ export function math() {
     answerInput.style.width = 10 + "%";
     document.body.append(questionBox);
     document.body.append(answerInput);
+
     if (userAnswer === correctAnswer) {
       score++;
       console.log(score);
